c16f9cfc795ef0b133d004edd8aff1576b750248
AgeOf <tylerecouture@gmail.com>
2016-12-21 10:11:21 -0800

    bulk import wngs and errors on students

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,5 @@
+import re
+
 import codecs
 import time
 
@@ -26,6 +28,8 @@ def mass_update(request):
 
     new_staff_list = []
     new_student_list = []
+    student_errors = []
+    staff_errors = []
     staff_import = False
     student_import = False
     form = UserImportForm(request.POST or None, request.FILES or None)
@@ -59,44 +63,61 @@ def mass_update(request):
             count = 0
             for row in reader:
 
-                # students should have one entry for each semester, only use semester indicated in form
-                # check if row has any elements first
-                if row and row[5] == semester:
-                    username = row[0]
-                    first_name = row[1]
-                    last_name = row[2]
-                    homeroom_teacher = row[3]
-                    grade = row[4]
-                    phone = row[6]
-                    email = row[7]
+                sn_regex_string = r"^(9[789])(\d{5})$"
 
-                    qs = User.objects.all()
-                    # check if user exists, else create new user
-                    if not qs.filter(username=username).exists():
-                        user = User.objects.create_user(
-                            username=username,
-                            password="wolf",
-                            first_name=first_name,
-                            last_name=last_name,
-                        )
-                        new_student_list.append(user.id)
+                # skip empty rows
+                if row:
+                    # check for student number
+                    if not re.match(sn_regex_string, row[0]):
+                        student_errors.append({'error': "Student number doesn't match pattern", 'row': row})
                     else:
-                        user = User.objects.get(username=username)
+                        # students should have one entry for each semester, only use semester indicated in form
+                        if row[5] == semester:
+                            username = row[0]
+                            first_name = row[1]
+                            last_name = row[2]
+                            homeroom_teacher = row[3]
+                            grade = row[4]
+                            phone = row[6]
+                            email = row[7]
 
-                    # A profile is created automatically when a new user is created.  Update it.
-                    # But sometimes errors cause a user to be created without a profile.  So just in case:
-                    profile, created = Profile.objects.get_or_create(user=user)
-                    try:  # sometimes homeroom id shows up as 0, which doesn't exist
-                        homeroom_teacher = User.objects.get(username=homeroom_teacher)
-                    except User.DoesNotExist:
-                        homeroom_teacher = None
-                    profile.homeroom_teacher = homeroom_teacher
-                    profile.grade = grade
-                    profile.phone = phone
-                    profile.email = email
-                    profile.save()
-                    count += 1
-                    print(str(count) + ": " + str(profile))
+                            # validate data
+                            try:  # sometimes homeroom id shows up as 0, which doesn't exist
+                                homeroom_teacher = User.objects.get(username=homeroom_teacher)
+                            except User.DoesNotExist:
+                                student_errors.append({'warning': "Homeroom teacher not recognized", 'row': row})
+                                homeroom_teacher = None
+                            if not grade:  # needs an int so empty string is bad
+                                grade = None
+                            if not phone:
+                                phone = None
+                            if not email:
+                                email = None
+
+                            qs = User.objects.all()
+                            # check if user exists, else create new user
+                            if not qs.filter(username=username).exists():
+                                user = User.objects.create_user(
+                                    username=username,
+                                    password="wolf",
+                                    first_name=first_name,
+                                    last_name=last_name,
+                                )
+                                new_student_list.append(user.id)
+                            else:
+                                user = User.objects.get(username=username)
+
+                            # A profile is created automatically when a new user is created.  Update it.
+                            # But sometimes errors cause a user to be created without a profile.  So just in case:
+                            profile, created = Profile.objects.get_or_create(user=user)
+                            homeroom_teacher = homeroom_teacher
+                            profile.homeroom_teacher = homeroom_teacher
+                            profile.grade = grade
+                            profile.phone = phone
+                            profile.email = email
+                            profile.save()
+                            count += 1
+                            print(str(count) + ": " + str(profile))
 
             new_student_list = User.objects.all().filter(id__in=new_student_list)
 
@@ -107,6 +128,7 @@ def mass_update(request):
         "staff_import": staff_import,
         "new_student_list": new_student_list,
         "student_import": student_import,
+        "student_errors": student_errors,
         "form": form,
     }
     return render(request, "profiles/profile_imports.html", context)
