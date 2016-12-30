ef05b7f2b60f6f3807c7cc7afef84432a2eafa7f
AgeOf <tylerecouture@gmail.com>
2016-12-30 13:06:20 -0800

    separate out SECRET KEY

--- src/flex/settings.py
+++ src/flex/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i@#(^ho8wu)6yvy^%w93^yh#cf^fs21)d&f29)lhz7gox%x%ta'
+SECRET_KEY = open(os.path.expanduser('~/.flex-secret')).read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
