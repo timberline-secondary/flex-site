a10aa3335fa6dccfc3c233c87729a6b02cf42319
AgeOf <tylerecouture@gmail.com>
2017-01-05 19:31:13 -0800

    add warning icon.

--- src/events/views.py
+++ src/events/views.py
@@ -385,7 +385,7 @@ def register(request, id, block_id):
 
         messages.success(request, "Successfully registered for <b>%s</b> during <b>%s</b> " % (event, block_text))
     else:
-        messages.warning(request, reason)
+        messages.warning(request, "<i class='fa fa-exclamation-triangle'></i> %s" % reason)
     return redirect("%s?date=%s" % (reverse('events:list_by_block', args=(block_id,)), date_query))
 
 
