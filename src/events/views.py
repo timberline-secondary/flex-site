6eac9ffb28f0b790de573fb63d406b37bd47412f
AgeOf <tylerecouture@gmail.com>
2017-01-05 19:29:21 -0800

    danger in bs, error in django.  Use warning.

--- src/events/views.py
+++ src/events/views.py
@@ -385,7 +385,7 @@ def register(request, id, block_id):
 
         messages.success(request, "Successfully registered for <b>%s</b> during <b>%s</b> " % (event, block_text))
     else:
-        messages.danger(request, reason)
+        messages.warning(request, reason)
     return redirect("%s?date=%s" % (reverse('events:list_by_block', args=(block_id,)), date_query))
 
 
