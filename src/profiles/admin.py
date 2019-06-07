a5c2e817f3ec6f5408e367298c1b15bd5fcb6db1
tylerecouture <tylerecouture@gmail.com>
2019-06-07 10:12:39 -0700

    reset permission records

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -6,9 +6,10 @@ from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ["user", "get_first_name", "get_last_name", "homeroom_teacher",
-                    "grade", "phone", "email", "updated", ]
+                    "grade", "phone", "email", "permission", "updated", ]
     readonly_fields = ["updated", ]
-    list_filter = ["grade", ]
+    list_filter = ["grade", "permission", ]
+    actions = ["reset_permission_form_records"]
     # list_editable = ["title", ]
     # list_display_links = ["created_timestamp", ]
 
@@ -26,6 +27,11 @@ class ProfileAdmin(admin.ModelAdmin):
         return obj.user.last_name
     get_last_name.short_description = 'Last Name'
     get_last_name.admin_order_field = 'user__last_name'
+
+    def reset_permission_form_records(self, request, queryset):
+        rows_updated = queryset.update(permission=None)
+        self.message_user(request, "Permissions records reset for {} students.".format(rows_updated))
+    reset_permission_form_records.short_description = "Reset permission records for selected students"
 
 
 admin.site.register(Profile, ProfileAdmin)
