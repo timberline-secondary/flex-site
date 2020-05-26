f2bcc74799a748acafb0b3474ccf67c543252c58
tylerecouture <tylerecouture@gmail.com>
2020-05-26 11:11:31 -0700

    change event dates from W to MTHF

--- src/events/static/js/events-datepicker.js
+++ src/events/static/js/events-datepicker.js
@@ -1,6 +1,7 @@
 function WednesdaysOnly(date) {
     var day = date.getDay();
-    if (day == 3) //Wednesday = 3
+    // no Sat, Sun, or Wed
+    if (day != 3 || day != 0 || day != 7) //Wednesday = 3
         return [true]
     else
         return [false]
--- src/flex/utils.py
+++ src/flex/utils.py
@@ -3,12 +3,26 @@ from datetime import date, timedelta
 
 def default_event_date():
     """
-    :return: today if Wednesday, else next Wednesday
+    # :return: today if Wednesday, else next Wednesday
+    # return next valid day
     """
+    # today = date.today()
+    # # Wednesday = 2
+    # wednesday = today + timedelta((2 - today.weekday()) % 7)
+    # return wednesday
+
+    # Next valid day is 0, 1, 3, 4 (M, T, Th, F)
     today = date.today()
-    # Wednesday = 2
-    wednesday = today + timedelta((2 - today.weekday()) % 7)
-    return wednesday
+    if today.weekday() == 2:  # Wed push to Thu
+        next_valid_day = today + timedelta(1)
+    elif today.weekday() == 5:  # Sat push to Mon
+        next_valid_day = today + timedelta(2)
+    elif today.weekday() == 6:  # Sun push to Mon
+        next_valid_day = today + timedelta(1)
+    else:
+        next_valid_day = today
+
+    return next_valid_day
 
 
 def school_year_start_date():
